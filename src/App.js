c8585ece5c25d69ec7dc73ef4b22aae5d5b1292f
Giannis Verginis <jverg@users.noreply.github.com>
2020-12-09 16:08:32 +0200

    Before lifecyrcle hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
-import React from "react";
-import "./styles.css";
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello Jverg</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+    console.log(counters);
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar
+          totalCounters={this.state.counters.filter((c) => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
+        </main>
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,28 +1,21 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-  state = {
-    count: 0
-  };
-
-  styles = {
-    fontWeight: "bold",
-    fontSize: 10
-  };
-
-  handleIncrement() {
-    console.log("Increment Clicked", this.state.count);
-  }
-
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
         </button>
       </div>
     );
@@ -30,13 +23,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : value;
   }
 }
 
