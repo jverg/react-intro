044748e9791e589ed84204fdaf1b87b35cb1c007
Giannis Verginis <jverg@users.noreply.github.com>
2020-12-11 11:45:04 +0200

    Stars refactored

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -39,7 +39,7 @@ function Counter(props) {
               height="1.2rem"
               viewBox="0 0 16 16"
               className="bi bi-basket2-fill"
-              fill="currentColor"
+              fill="white"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
@@ -57,7 +57,7 @@ function Counter(props) {
               height="1.2em"
               viewBox="0 0 16 16"
               className="bi bi-trash"
-              fill="currentColor"
+              fill="white"
               xmlns="http://www.w3.org/2000/svg"
             >
               <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
