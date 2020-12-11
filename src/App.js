05fd29f088d49b2deb36b0eb6c98024a5e4b2683
Giannis Verginis <jverg@users.noreply.github.com>
2020-12-11 09:08:56 +0200

    Refactor class to function

--- src/App.js
+++ src/App.js
@@ -1,74 +1,65 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import NavBar from "./components/navbar";
 import Counters from "./components/counters";
 
-class App extends Component {
-  state = {
-    totalCounter: 0,
-    stars: [1, 2, 3, 4, 5],
-    counters: [
-      { id: 1, name: "Λαμπάκια", stars: 0, value: 0 },
-      { id: 2, name: "Χριστουγεννιάτικο δέντρο", stars: 0, value: 0 },
-      { id: 3, name: "Φάτνη", stars: 0, value: 0 },
-      { id: 4, name: "Σοκολατάκια", stars: 0, value: 0 },
-      { id: 5, name: "Άγιος Βασίλης", stars: 0, value: 0 },
-      { id: 6, name: "Μελομακάρονα", stars: 0, value: 0 },
-      { id: 7, name: "Κουραμπιέδες", stars: 0, value: 0 }
-    ]
-  };
+function App() {
+  const [totalCounter, setTotalCounter] = useState(0);
+  const [stars, setStars] = useState([1, 2, 3, 4, 5]);
+  const [counters, setCounters] = useState([
+    { id: 1, name: "Λαμπάκια", stars: 0, value: 0 },
+    { id: 2, name: "Χριστουγεννιάτικο δέντρο", stars: 0, value: 0 },
+    { id: 3, name: "Φάτνη", stars: 0, value: 0 },
+    { id: 4, name: "Σοκολατάκια", stars: 0, value: 0 },
+    { id: 5, name: "Άγιος Βασίλης", stars: 0, value: 0 },
+    { id: 6, name: "Μελομακάρονα", stars: 0, value: 0 },
+    { id: 7, name: "Κουραμπιέδες", stars: 0, value: 0 }
+  ]);
 
-  handleDelete = (counter) => {
-    const counters = [...this.state.counters];
+  const handleDelete = (counter) => {
     const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    const totalCounter = this.state.totalCounter - counters[index].value;
+    setTotalCounter(totalCounter - counters[index].value);
     counters[index].value = 0;
-    this.setState({ counters, totalCounter });
+    setCounters(counters);
   };
 
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
+  const handleIncrement = (counter) => {
     const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
     counters[index].value++;
-    const totalCounter = this.state.totalCounter + 1;
-    this.setState({ counters, totalCounter });
+    setTotalCounter(totalCounter + 1);
+    setCounters(counters);
   };
 
-  starUpdate = (counter, star) => {
-    const counters = [...this.state.counters];
+  const starUpdate = (counter, star) => {
     const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
     counters[index].stars = 0 + star;
-    this.setState({ counters });
+    setCounters(counters);
+    setStars([1, 2, 3, 4, 5]);
   };
 
-  handleReset = () => {
-    const totalCounter = 0;
-    const counters = this.state.counters.map((c) => {
+  const handleReset = () => {
+    setTotalCounter(0);
+    const countersIn = counters.map((c) => {
       c.value = 0;
       return c;
     });
-    this.setState({ counters, totalCounter });
+    setCounters(countersIn);
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <NavBar totalCounter={this.state.totalCounter} />
-        <main className="container">
-          <Counters
-            counters={this.state.counters}
-            stars={this.state.stars}
-            onReset={this.handleReset}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
-            onStarUpdate={this.starUpdate}
-          />
-        </main>
-      </React.Fragment>
-    );
-  }
+  return (
+    <React.Fragment>
+      <NavBar totalCounter={totalCounter} />
+      <main className="container">
+        <Counters
+          counters={counters}
+          stars={stars}
+          onReset={handleReset}
+          onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onStarUpdate={starUpdate}
+        />
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default App;
