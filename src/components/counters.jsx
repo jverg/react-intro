1a9196902b74a358730b044fe71bb9bc057f4e9d
Giannis Verginis <jverg@users.noreply.github.com>
2020-12-11 10:23:31 +0200

    Countrs refactored

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,42 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./counter";
 
-class Counters extends Component {
-  render() {
-    const {
-      onReset,
-      counters,
-      stars,
-      onDelete,
-      onIncrement,
-      onStarUpdate
-    } = this.props;
-
-    return (
-      <div className="col-md-12">
-        <hr />
-        <button className="btn btn-primary btn-sm.m-2" onClick={onReset}>
-          Αφαίρεση όλων
-        </button>
-        <hr />
-        <div className="container">
-          <div className="row">
-            {counters.map((counter) => (
-              <Counter
-                key={counter.id}
-                stars={stars}
-                onDelete={onDelete}
-                onIncrement={onIncrement}
-                onStarUpdate={onStarUpdate}
-                counters={counters}
-                counter={counter}
-              />
-            ))}
-          </div>
+function Counters(props) {
+  return (
+    <div className="col-md-12">
+      <hr />
+      <button className="btn btn-primary btn-sm.m-2" onClick={props.onReset}>
+        Αφαίρεση όλων
+      </button>
+      <hr />
+      <div className="container">
+        <div className="row">
+          {props.counters.map((counter) => (
+            <Counter
+              key={counter.id}
+              stars={props.stars}
+              onDelete={props.onDelete}
+              onIncrement={props.onIncrement}
+              onStarUpdate={props.onStarUpdate}
+              counters={props.counters}
+              counter={counter}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Counters;
